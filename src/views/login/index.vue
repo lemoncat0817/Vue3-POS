73be76bbc361d6c290690fd28d413eb228ec025d
lemoncat0817 <king123456789abc@gmail.com>
2024-06-11 16:46:20 +0800

    新增教學影片

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,17 @@
 <template>
   <div
     class="w-screen 2xl:h-screen xl:h-[800px] h-[959px] lg:h-[768px] bg-[url('@/assets/login-Bg.png')] bg-no-repeat bg-[length:100%_100%] text-white relative">
-    <div
+    <div v-if="isWatchVideo" class="absolute top-[40%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col items-center xl:gap-[20px] lg:gap-[15px] gap-[10px]g">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/4ELxt64heEs?si=V5_55DrBO2G1kN0L"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+      <button
+        class="border-2 border-black border-solid rounded-lg p-1 ml-1 bg-red-500 text-blue-700 font-bold hover:bg-red-600 active:bg-yellow-400"
+        @click="isWatchVideo = !isWatchVideo">回到登入頁面</button>
+    </div>
+    <div v-if="!isWatchVideo"
       class="absolute top-[40%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col items-center xl:gap-[20px] lg:gap-[15px] gap-[10px]">
       <div class="flex items-center gap-[1px]">
         <h1
@@ -11,6 +21,9 @@
         </h1>
         <img src="@/assets/logo.png" alt="logo"
           class="w-16 h-4/5 rounded-lg ml-2 border-2 border-black border-solid lg:w-14 hover:scale-[1.1] hover:animate-bounce ">
+        <button
+          class="border-2 border-black border-solid rounded-lg p-1 ml-1 bg-red-500 text-blue-700 font-bold hover:bg-red-600 active:bg-yellow-400"
+          @click="isWatchVideo = !isWatchVideo">觀看教學影片</button>
       </div>
       <div class="flex items-center gap-[10px]">
         <p class="text-[15px] font-bold text-red-400 sm:text-[20px]">帳號</p>
@@ -27,7 +40,7 @@
         <button @click="login"
           class="w-[100px] xl:h-12 lg:h-10 h-8 leading-[8px] text-center rounded-[20px] p-2 bg-[#cc191f] text-center cursor-pointer font-bold lg:w-[80px] hover:scale-[1.3] hover:w-[150px] transition-all duration-500 ml-5 hover:bg-[#ff4500] hover:text-blue-800">登入</button>
       </div>
-      <p class="sm:text-lg text-base font-bold text-red-300">快速登入(測試時使用,實際使用會移除): </p>
+      <p class="sm:text-lg text-base font-bold text-red-300">快速登入(測試時使用,實際使用會移除)</p>
       <div class="flex justify-between w-[300px]">
         <button
           class="border-2 border-black border-solid rounded-lg px-2 bg-blue-500 text-center font-bold text-red-200 text-lg lg:text-base hover:bg-blue-800 hover:scale-[1.1] active:bg-yellow-700"
@@ -44,6 +57,7 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
 import { useLoginStore } from '@/stores/login'
@@ -51,6 +65,9 @@ const loginStore = useLoginStore()
 import { useAuthorityManagementStore } from '@/stores/authorityManagement'
 const authorityManagementStore = useAuthorityManagementStore()
 import { ElMessage, ElNotification } from 'element-plus'
+
+// 教學影片相關功能
+const isWatchVideo = ref(false)
 
 // 快速登入相關功能
 const quicklyLogin = (num) => {
